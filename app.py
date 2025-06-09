07a8eb7942ac167abffbd81e1b5bdb3d48ceb0cf
Joshua Hallam <joshua.hallam127@gmail.com>
2025-06-09 20:04:21 +1000

    yeah

--- app.py
+++ app.py
@@ -83,9 +83,10 @@ def get_average_train_delays():
         start_date = request.args.get('start_date')
         end_date = request.args.get('end_date')
         sydney_only = request.args.get('sydney_only') == 'true'
+        shit_type = 'early' if request.args.get('shit_type') == 'early' else 'delay'
 
-        query = """
-            SELECT routes.description, ROUND(SUM(rdd.total_delay) / SUM(rdd.total_count), 2) AS avg_delay, SUM(rdd.hits)
+        query = f"""
+            SELECT routes.description, ROUND(SUM(rdd.total_{shit_type}) / SUM(rdd.total_count), 2) AS avg_delay, SUM(rdd.hits)
             FROM route_daily_delays rdd
             JOIN routes ON rdd.route_short_name = routes.short_name
             WHERE rdd.date BETWEEN %s AND %s
@@ -119,9 +120,10 @@ def get_average_train_stop_delays():
         start_date = request.args.get('start_date')
         end_date = request.args.get('end_date')
         sydney_only = request.args.get('sydney_only') == 'true'
+        shit_type = 'early' if request.args.get('shit_type') == 'early' else 'delay'
 
-        query = """
-            SELECT sdd.route_short_name, stops.name, ROUND(SUM(sdd.total_delay) / SUM(sdd.total_count), 2) AS avg_delay, SUM(sdd.hits)
+        query = f"""
+            SELECT sdd.route_short_name, stops.name, ROUND(SUM(sdd.total_{shit_type}) / SUM(sdd.total_count), 2) AS avg_delay, SUM(sdd.hits)
             FROM stop_daily_delays sdd
             JOIN stops ON sdd.stop_id = stops.id
             JOIN routes ON sdd.route_short_name = routes.short_name
