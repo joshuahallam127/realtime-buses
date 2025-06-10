ccd9acea8d5e976c9db56025e69e13b4c4d84024
Joshua Hallam <joshua.hallam127@gmail.com>
2025-06-11 01:34:39 +1000

    migration

--- buses/migration.py
+++ buses/migration.py
@@ -1,6 +1,11 @@
 from functions import get_connection
 
 conn, cursor = get_connection()
-cursor.execute("""ALTER TABLE delays
-    DROP COLUMN IF EXISTS date
-""")
+cursor.execute("""ALTER TABLE delays DROP COLUMN start_date""")
+cursor.execute("""CREATE TABLE cancels (
+    trip_id VARCHAR(9) PRIMARY KEY,
+    route_id VARCHAR(10) REFERENCES routes(id)
+)""")
+conn.commit()
+cursor.close()
+conn.close()
